1cfe40cbdb56a012e2ef3d3599f908ff39e0d7f0
Muhammad Ikhsan <88651883+Ikhsans4@users.noreply.github.com>
2022-02-12 21:02:35 +0700

    pertemuan 2

---
+++ Pertemuan 2/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+
+    // deklarasi variabel
+    int angka1;
+    int angka2 = 3;
+    angka2 += 3;
+    angka2 = angka2 + 3;
+    float angka3 = 10.2;
+    float hasil = angka3 / angka2;
+    // printf("angka 1 = %d\n", angka2);
+    printf("angka 2 = %f", hasil);
+    printf("angka 3 = %.3f\n", angka3);
+
+    return 0;
+}
