f4d1b0a9bf676cf8efbf7f03c90eee39127acdcd
ikhsan <m_ikhsan20@mhs.unsyiah.ac.id>
2022-03-18 15:37:33 +0700

    tambahan

--- Pertemuan 6/fString.c
+++ Pertemuan 6/fString.c
@@ -1,16 +1,13 @@
 #include <stdio.h>
-#include <string.h>
+#include <String.h> //penting
+
 int main()
 {
-    char kata1[] = "Whatsap ngelag";
-    char kata2[20];
-    printf("sebelum dicopy\n");
-    printf("isi kata 1 : %s\n", kata1);
-    printf("isi kata 2 : %s\n", kata2);
-    strcpy(kata2, kata1);
-    printf("setelah dicopy\n");
-    printf("isi kata 1 : %s\n", kata1);
-    printf("isi kata 2 : %s\n", kata2);
+
+    char kalimat[10] = "ini ani";
+
+    // menghitung panjang kalimat
+    printf("panjang kalimat : %d", strlen(kalimat));
 
     return 0;
 }
